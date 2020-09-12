579f65833bcdd201471a4d7212f0376ba6776b1d
yuki0225 <67815365+yukk000@users.noreply.github.com>
2020-09-12 12:17:45 +0900

    Update write.php

--- Newkeijiban/public/bbs/write.php
+++ Newkeijiban/public/bbs/write.php
@@ -1,35 +1,27 @@
 
 <?php
-// 必須項目の本文のどちらでも欠けていたら何も処理せず閲覧画面に戻す (リダイレクト)
 if (empty($_POST['body'])) {
   header("HTTP/1.1 302 Found");
   header("Location: ./read.php");
   return;
 }
 
-// ログインしていなければ閲覧画面に戻す(リダイレクト)
 if (empty($_COOKIE["login_id"])) {
   header("HTTP/1.1 302 Found");
   header("Location: ./read.php");
   return;
 }
 
-// データベースハンドラ作成
 $dbh = new PDO('mysql:host=mysql;dbname=2020techc_db', '2020techc_username', '2020techc_password');
 
-// 投稿保存用テーブル bbs_entries に1行insert
-// SQLインジェクションを防ぐためにプレースホルダを使う
 $insert_sth = $dbh->prepare("INSERT INTO bbs_entries (name, body) VALUES (:name, :body)");
 $insert_sth->execute([
     ':name' => $_COOKIE["login_id"],
     ':body' => $_POST['body'],
 ]);
 
-
-// 覚えさせる
 setcookie("name", $_POST['name']);
 
-// 書き込み完了したら閲覧画面に戻す
 header("HTTP/1.1 302 Found");
 header("Location: ./read.php");
 return;
