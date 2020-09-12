f656bcd2e577816f9aabf0cff1fde4f112fe1ff3
yuki0225 <67815365+yukk000@users.noreply.github.com>
2020-09-12 11:22:59 +0900

    Update register.php

--- Newkeijiban/public/users/register.php
+++ Newkeijiban/public/users/register.php
@@ -1,1 +1,23 @@
 
+<?php
+
+// なにもしない
+
+?>
+
+<head>
+  <title>会員登録</title>
+</head>
+<body>
+  <h1>会員登録</h1>
+
+  <form method="POST" action="./register_process.php" style="margin: 2em;">
+    <div>
+      ログインID: <input type="text" name="login_id">
+    </div>
+    <div>
+      パスワード: <input type="password" name="password">
+    </div>
+    <button type="submit">決定</button>
+  </form>
+</body>
