b08821c792eb477720877df7367b1c9d57e5c127
yuki0225 <67815365+yukk000@users.noreply.github.com>
2020-09-12 11:21:37 +0900

    Update login_finish.php

--- Newkeijiban/public/users/login_finish.php
+++ Newkeijiban/public/users/login_finish.php
@@ -1,1 +1,12 @@
 
+<html>
+<head>
+  <title>ログイン 完了</title>
+</head>
+<body>
+  <h1>ログイン 完了</h1>
+  <p>
+    ログインid: <?= htmlspecialchars($_COOKIE["login_id"]) ?> でログインできました。<br>
+    <a href="/bbs/read.php">掲示板へ</a>
+  </p>
+</body>
