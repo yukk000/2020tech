564ae49bdba2aca30cab05310be18ee21cdc1bb8
yuki0225 <67815365+yukk000@users.noreply.github.com>
2020-09-12 11:21:12 +0900

    Update login.php

--- Newkeijiban/public/users/login.php
+++ Newkeijiban/public/users/login.php
@@ -1,1 +1,18 @@
 
+<head>
+  <title>ログイン</title>
+</head>
+<body>
+  <h1>ログイン</h1>
+  <form method="POST" action="./login_process.php" style="margin: 2em;">
+    <div>
+      ログインID: <input type="text" name="login_id">
+    </div>
+    <div>
+      パスワード: <input type="password" name="password">
+    </div>
+    <button type="submit">決定</button>
+  </form>
+  <hr>
+  <a href="./register.php">会員登録がまだのひとはこちらから会員登録してください。</a>
+</body>
