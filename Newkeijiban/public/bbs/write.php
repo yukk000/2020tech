be59abe8685817413bc600aa168155abfd29f0d1
yuki0225 <67815365+yukk000@users.noreply.github.com>
2021-02-05 22:17:50 -0900

    Update write.php

--- Newkeijiban/public/bbs/write.php
+++ Newkeijiban/public/bbs/write.php
@@ -1,28 +1,51 @@
-
 <?php
-if (empty($_POST['body'])) {
+/* セッションについて共通処理 ここから */
+$redis = new Redis();
+$redis->connect("redis", 6379);
+$session_id_cookie_key = "session_id";
+$session_id = isset($_COOKIE[$session_id_cookie_key]) ? ($_COOKIE[$session_id_cookie_key]) : null;
+if ($session_id === null) {
+    $session_id = bin2hex(random_bytes(25));
+    setcookie($session_id_cookie_key, $session_id, 0, '/');
+}
+$redis_session_key = "session-" . $session_id;
+$session_values = $redis->exists($redis_session_key)
+    ? json_decode($redis->get($redis_session_key), true)
+    : []; 
+/* ここまで */
+
+$csrf_tokens = isset($session_values["csrf_tokens"]) ? $session_values["csrf_tokens"] : []; 
+if (!in_array($_POST['csrf_token'], $csrf_tokens, true)) {
   header("HTTP/1.1 302 Found");
   header("Location: ./read.php");
   return;
 }
 
-if (empty($_COOKIE["login_id"])) {
-  header("HTTP/1.1 302 Found");
-  header("Location: ./read.php");
-  return;
-}
-
+// データベースハンドラ作成
 $dbh = new PDO('mysql:host=mysql;dbname=2020techc_db', '2020techc_username', '2020techc_password');
 
-$insert_sth = $dbh->prepare("INSERT INTO bbs_entries (name, body) VALUES (:name, :body)");
-$insert_sth->execute([
-    ':name' => $_COOKIE["login_id"],
-    ':body' => $_POST['body'],
+// 主キーのIDが一致する1行だけ取得
+$select_user_sth = $dbh->prepare('SELECT id, login_id, password FROM users WHERE id = :id LIMIT 1');
+$select_user_sth->execute([
+    ':id' => $session_values["login_user_id"],
 ]);
+$login_user = $select_user_sth->fetch();
 
-setcookie("name", $_POST['name']);
+if (empty($login_user) || empty($_POST['body'])) { 
+  header("HTTP/1.1 302 Found");
+  header("Location: ./read.php");
+  return;
+}
+
+// bbs_entries テーブルに1行insert
+$insert_sth = $dbh->prepare("INSERT INTO bbs_entries (name, body, user_id) VALUES (:name, :body, :user_id)");
+$insert_sth->execute([
+    ':name' => $login_user["login_id"],
+    ':body' => $_POST['body'],
+    ':user_id' => $login_user["id"],
+]);
 
 header("HTTP/1.1 302 Found");
 header("Location: ./read.php");
 return;
-?>
+?>          
